08de3d85477f29bdaa0763cc78e8a41055d8bbdc
Felix <felix.gaillard51@gmail.com>
2022-01-17 15:09:06 +0100

    mapping Jpa Topic

---
+++ src/main/java/dev/entity/Topic.java
@@ -0,0 +1,74 @@
+package dev.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Topic {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String title;
+	private String text;
+	private String image;
+	private LocalDate date;
+
+	@OneToMany(mappedBy = "topic")
+	private List<Tag> tag;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public List<Tag> getTag() {
+		return tag;
+	}
+
+	public void setTag(List<Tag> tag) {
+		this.tag = tag;
+	}
+}
---
+++ src/main/java/dev/repository/TopicRepository.java
@@ -0,0 +1,10 @@
+package dev.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dev.entity.Topic;
+
+public interface TopicRepository extends JpaRepository<Topic, Integer> {
+
+
+}
