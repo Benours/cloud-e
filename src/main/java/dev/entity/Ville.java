5f6e67a612928196182c7a43a8015ea83334cb92
Baska Benjamin <benjamin.baska@gmail.com>
2022-01-21 13:45:01 +0100

    fix

--- src/main/java/dev/entity/Ville.java
+++ src/main/java/dev/entity/Ville.java
@@ -11,6 +11,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class Ville {
 	@Id
@@ -23,6 +25,7 @@ public class Ville {
 
 	
 	@ManyToMany(mappedBy = "villes")
+	@JsonIgnore
 	private List<User> users;
 	
 	public Integer getId() {
